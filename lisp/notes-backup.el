d7079d7c8b644449e5774be228c289fdd6bdf028
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-15 16:35:34 +0100

    .

--- lisp/notes-backup.el
+++ lisp/notes-backup.el
@@ -7,13 +7,23 @@
 ;;; Code:
 (defcustom laat/notes-backup-script nil
   "The backup script."
-  :type 'file)
+  :type 'file
+  :group 'notes-backup)
 
 ;;;###autoload
 (defun laat/execute-backup-script ()
   "Execute the backup script."
   (interactive)
-  (when laat/notes-backup-script (shell-command laat/notes-backup-script)))
+  (when laat/notes-backup-script
+    (let* ((file-name (shell-quote-argument laat/notes-backup-script))
+           (process (start-file-process-shell-command "notes-backup" "* notes-backup *" file-name)))
+      (set-process-sentinel process
+                            (lambda (process event)
+                              (message (concat "notes-backup: " event))
+                              (cond ((string-match-p "finished" event)
+                                     (progn
+                                       (kill-buffer "* notes-backup *")
+                                       (message "notes-backup: complete")))))))))
 
 ;;;###autoload
 (run-with-idle-timer 300 :repeat 'laat/execute-backup-script)

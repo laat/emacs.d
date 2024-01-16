55ccbad4730dbbbd4e58fba5c6d749ddbd20f586
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-16 17:03:56 +0100

    .

--- lisp/notes-backup.el
+++ lisp/notes-backup.el
@@ -1,29 +1,31 @@
-;;; package -- Summary
-
+;;; flyspell-correct.el --- Correcting words with flyspell via custom interface  -*- lexical-binding: t; -*-
+;;
 ;;; Commentary:
 
 ;; A simple "cron-job" that executes a backup script
 
 ;;; Code:
-(defcustom laat/notes-backup-script nil
+(defcustom laat/notes-backup-scripts nil
   "The backup script."
-  :type 'file
+  :type '(repeat :tag "List of sync scripts" file)
   :group 'notes-backup)
 
 ;;;###autoload
 (defun laat/execute-backup-script ()
   "Execute the backup script."
   (interactive)
-  (when laat/notes-backup-script
-    (let* ((file-name (shell-quote-argument laat/notes-backup-script))
-           (process (start-file-process-shell-command "notes-backup" "* notes-backup *" file-name)))
+  (dolist (script laat/notes-backup-scripts)
+    (let* ((file-name (shell-quote-argument script))
+           (buff-name (concat "*notes-backup " file-name "*"))
+           (process (start-file-process-shell-command "notes-backup" buff-name file-name)))
       (set-process-sentinel process
-                            (lambda (process event)
-                              (message (concat "notes-backup: " event))
+                            (lambda (_ event)
+                              (message (concat "notes-backup " file-name " " event))
                               (cond ((string-match-p "finished" event)
                                      (progn
-                                       (kill-buffer "* notes-backup *")
-                                       (message "notes-backup: complete")))))))))
+                                       (kill-buffer buff-name)
+                                       (message (concat "notes-backup: " file-name " complete"))))))))))
+
 
 ;;;###autoload
 (run-with-idle-timer 300 :repeat 'laat/execute-backup-script)
